40a5a2807bc2113f7e0cf002fe0b562107a625fd
Joshua Allison <63320691+joshua-allison@users.noreply.github.com>
2022-03-06 15:52:20 -0800

    Create TODO.cs
    Added for easy task tracking

---
+++ TheDigitalToolbox/TODO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheDigitalToolbox
+{
+    public class TODO
+    {
+        //I believe that TODO's that are stored in CSHTML files aren't viewable in the Task List, so instead I'll store them, and any other homeless tasks here...
+
+        /*  TODO 4 Views>Shared>Layout.cshtml: Style
+        *       Add custom style sheet, rather than using the blue and white default of ASP.NET MVC Core
+        *   TODO 2 Views>Shared>Layout.cshtml: Navbar
+        *       Fix the janky-ness of the navbar layout
+        *   TODO 1 Add Authorization/Authentication
+        *       Decorate Model Controller action methods with appropriate Authentication/Authorization
+        *       
+        *   TODO 3 Host Site 
+        *   TODO 40 Optimize Site
+        *   TODO 41 Fully Async Site
+        */
+    }
+}
