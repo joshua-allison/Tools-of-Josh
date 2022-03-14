c5cd1126b01ca91e22e8bdd77378454826808ab3
Joshua Allison <63320691+joshua-allison@users.noreply.github.com>
2022-03-13 23:15:37 -0700

    Migration Successful
    The migration looks good, and it does what I want it to do. Updating database...

--- TheDigitalToolbox/Migrations/20220314061453_Tool-PK.Designer.cs
+++ TheDigitalToolbox/Migrations/20220314061453_Tool-PK.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TheDigitalToolbox.Models;
 
 namespace TheDigitalToolbox.Migrations
 {
     [DbContext(typeof(ToolboxContext))]
-    partial class ToolboxContextModelSnapshot : ModelSnapshot
+    [Migration("20220314061453_Tool-PK")]
+    partial class ToolPK
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
